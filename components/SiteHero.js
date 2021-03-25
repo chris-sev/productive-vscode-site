f09af84a3cdb62892476958a859309a0b33dcd9b
Chris Sev <chris@better.dev>
2021-03-25 13:22:28 -0700

    responsive work

--- components/SiteHero.js
+++ components/SiteHero.js
@@ -3,15 +3,15 @@ import GetSamplesForm from "./GetSamplesForm";
 
 export default function SiteHero() {
   return (
-    <div className="mx-8 py-28 text-center relative bg-gradient-to-br from-indigo-600 via-purple-700 to-purple-900 rounded-lg overflow-hidden flex items-center justify-center">
+    <div className="mx-8 lg:py-28 text-center relative bg-gradient-to-br from-indigo-600 via-purple-700 to-purple-900 rounded-lg overflow-hidden flex items-center justify-center">
       {/* content */}
-      <div className="flex flex-col justify-center p-20 space-y-12 w-full">
+      <div className="flex flex-col justify-center py-12 px-5 lg:py-20 space-y-12 w-full">
         {/* giant title */}
         <h1 className="leading-none text-white font-extrabold fancy">
           <span className="block text-xl lg:text-4xl mb-3 lg:mb-5 uppercase tracking-wider text-purple-200">
             Productive
           </span>{" "}
-          <span className="block text-4xl lg:text-9xl tracking-tigher">
+          <span className="block text-5xl lg:text-9xl tracking-tigher">
             VS Code
           </span>
         </h1>
