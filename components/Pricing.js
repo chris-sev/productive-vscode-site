2e59880fa1bc3139df0f6f9c7b25ad3bbd505ddf
Chris Sev <chris.sevilleja@gmail.com>
2021-08-03 14:26:11 -0700

    pricing changes

--- components/Pricing.js
+++ components/Pricing.js
@@ -6,10 +6,10 @@ const plans = [
     title: "Starter Package",
     featured: false,
     description: "All your essential business finances, taken care of.",
-    price: 47,
+    price: 45,
     priceMonthlyPlan: 16,
     mainFeatures: [
-      { id: 1, value: "Basic invoicing" },
+      { id: 1, value: "Access to the " },
       { id: 2, value: "Easy to use accounting" },
       { id: 3, value: "Mutli-accounts" },
     ],
@@ -18,7 +18,7 @@ const plans = [
     title: "Complete Package",
     featured: true,
     description: "The best financial services for your thriving business.",
-    price: 60,
+    price: 59,
     priceMonthlyPlan: 20,
     mainFeatures: [
       { id: 1, value: "Advanced invoicing" },
@@ -43,12 +43,12 @@ export default function Pricing({ coupon, discount, price }) {
     <>
       <Script src="https://cdn.podia.com/embeds.js" />
 
-      <div id="pricing" className="py-56 bg-indigo-600">
+      <div id="get" className="py-56 bg-purple-600">
         <div className="relative px-8 max-w-7xl mx-auto">
           <h1 className="text-center text-8xl font-extrabold tracking-tight text-white">
             Get the Course
           </h1>
-          <p className="text-center mt-6 text-3xl text-indigo-100">
+          <p className="text-center mt-6 text-3xl text-purple-100">
             Start making VS Code work for you.
           </p>
 
@@ -61,14 +61,14 @@ export default function Pricing({ coupon, discount, price }) {
             />
 
             {/* loop over the plans */}
-            <div className="relative space-y-6 lg:space-y-0 lg:grid lg:grid-cols-6">
+            <div className="relative space-y-6 lg:space-y-0 lg:mx-auto lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-x-5">
               {plans.map((plan) => (
                 <div
                   key={plan.title}
                   className={classNames(
                     plan.featured
-                      ? "col-span-2 bg-white ring-2 ring-indigo-700 shadow-md rounded-lg"
-                      : "col-span-2 col-start-2 bg-indigo-500 my-4 rounded-l-lg",
+                      ? "bg-white ring-4 ring-yellow-400 shadow-md rounded-lg"
+                      : "bg-purple-500 my-4 rounded-lg",
                     "pt-6 px-6 pb-6 lg:pb-8 lg:px-8 lg:pt-12"
                   )}
                 >
@@ -76,7 +76,7 @@ export default function Pricing({ coupon, discount, price }) {
                     {/* header */}
                     <h3
                       className={classNames(
-                        plan.featured ? "text-indigo-600" : "text-white",
+                        plan.featured ? "text-purple-600" : "text-white",
                         "text-xl font-semibold uppercase tracking-wide"
                       )}
                     >
@@ -85,10 +85,10 @@ export default function Pricing({ coupon, discount, price }) {
 
                     {/* price */}
                     <div className="flex flex-col items-start sm:flex-row sm:items-center sm:justify-between lg:flex-col lg:items-start">
-                      <div className="mt-5 space-y-4 items-center">
+                      <div className="mt-3 space-y-4 items-center">
                         <p
                           className={classNames(
-                            plan.featured ? "text-indigo-600" : "text-white",
+                            plan.featured ? "text-purple-600" : "text-white",
                             "text-6xl font-extrabold tracking-tight"
                           )}
                         >
@@ -97,7 +97,7 @@ export default function Pricing({ coupon, discount, price }) {
 
                         <p
                           className={classNames(
-                            plan.featured ? "text-gray-500" : "text-indigo-200",
+                            plan.featured ? "text-gray-500" : "text-purple-200",
                             "text-sm"
                           )}
                         >
@@ -106,25 +106,20 @@ export default function Pricing({ coupon, discount, price }) {
                       </div>
                     </div>
                   </div>
+
+                  {/* plan features ====================================== */}
                   <h4 className="sr-only">Features</h4>
-                  <ul
-                    className={classNames(
-                      plan.featured
-                        ? "border-gray-200 divide-gray-200"
-                        : "border-indigo-500 divide-indigo-500 divide-opacity-75",
-                      "mt-7 border-t divide-y lg:border-t-0"
-                    )}
-                  >
+                  <ul className={classNames(plan.featured ? "" : "", "mt-10")}>
                     {plan.mainFeatures.map((mainFeature) => (
                       <li
                         key={mainFeature.id}
-                        className="py-3 flex items-center"
+                        className="py-2 flex items-center"
                       >
                         <BsCheckCircle
                           className={classNames(
                             plan.featured
-                              ? "text-indigo-500"
-                              : "text-indigo-200",
+                              ? "text-purple-500"
+                              : "text-purple-200",
                             "w-5 h-5 flex-shrink-0"
                           )}
                           aria-hidden="true"
@@ -141,29 +136,46 @@ export default function Pricing({ coupon, discount, price }) {
                     ))}
                   </ul>
 
+                  {/* button */}
                   <div
                     onClick={trackForFathom}
                     dangerouslySetInnerHTML={{
                       __html: `
-                        <a
-                          href="https://learn.better.dev/productive-vs-code-starter?coupon=${coupon}"
-                          data-podia-embed="link"
-                          class="${classNames(
-                            plan.featured
-                              ? "bg-indigo-600 text-white hover:bg-indigo-700"
-                              : "bg-white text-indigo-600 hover:bg-indigo-50",
-                            "mt-6 w-full inline-block py-5 px-8 border border-transparent rounded-md shadow-sm text-center text-xl font-medium sm:mt-0 sm:w-auto lg:mt-6 lg:w-full"
-                          )}"
-                        >
-                          Buy the ${plan.title}
-                        </a>
-                      `,
+                      <a
+                        href="https://learn.better.dev/productive-vs-code-starter?coupon=${coupon}"
+                        data-podia-embed="link"
+                        class="${classNames(
+                          plan.featured
+                            ? "bg-yellow-300 text-purple-900 hover:bg-yellow-200"
+                            : "bg-purple-400 text-purple-50 hover:bg-purple-300",
+                          "mt-10 w-full inline-block py-5 px-8 rounded-md shadow-sm text-center text-xl font-medium sm:w-auto lg:w-full"
+                        )}"
+                      >
+                        Buy the ${plan.title}
+                      </a>
+                    `,
                     }}
                   />
+                  <p
+                    className={`mt-4 text-sm text-center ${
+                      plan.featured ? "text-gray-800" : "text-purple-200"
+                    }`}
+                  >
+                    Lifetime Refund Guarantee
+                  </p>
                 </div>
               ))}
             </div>
           </div>
+
+          {/* guarantee */}
+          <p className="mt-12 text-purple-200 text-center">
+            <strong className="font-bold text-purple-100 block mb-1">
+              Lifetime Refund Guarantee
+            </strong>{" "}
+            If you're ever unhappy with the course (even in the year 3050), get
+            a full refund no questions asked.
+          </p>
         </div>
       </div>
     </>
